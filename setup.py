71382c8200a7fb659cb57af8e3bf29efc0f3f105
Owen Jacobson <owen@freshbooks.com>
2012-03-08 14:38:50 -0500

    Updated version number for release (1.6).

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='refreshbooks',
-    version='1.5.1.dev',
+    version='1.6',
     author='Owen Jacobson',
     author_email='api@freshbooks.com',
     url='http://github.com/freshbooks/refreshbooks',
