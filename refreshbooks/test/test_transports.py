72061b51a0a9e506278f1e3a2c9d3e9814b0a12c
Taavi Burns <taavi@freshbooks.com>
2012-04-05 16:13:57 -0400

    On the other hand, outright integration tests seem a lot cleaner, and get us more value. Also including testing instructions in the readme.

--- refreshbooks/test/test_transports.py
+++ refreshbooks/test/test_transports.py
@@ -1,57 +1,54 @@
 from mock import patch, Mock, sentinel
 from nose.tools import raises
+from nose.plugins.attrib import attr
+from nose.plugins.skip import SkipTest
 from refreshbooks.exceptions import TransportException
-from urllib2 import HTTPError
 
-@patch('refreshbooks.transports.use_urllib2.u')
-def test_urllib2_transport_exception(u):
-    class FakeHttpError(HTTPError):
-        def __init__(self, code, content):
-            self.code = code
-            self.content = content
+@attr('integration')
+@raises(TransportException)
+def test_urllib2_transport_exception():
+    from refreshbooks.transports.use_urllib2 import Transport
+    Transport('http://httpstat.us/400', dict)("foo")
 
-        def read(self):
-            return self.content
+@attr('integration')
+def test_urllib2():
+    from refreshbooks.transports.use_urllib2 import Transport
+    assert len(Transport('http://httpstat.us/200', dict)("foo")) > 0
 
-    u.HTTPError = FakeHttpError
-    u.urlopen.return_value = Mock()
-    u.urlopen.return_value.read.side_effect = FakeHttpError(sentinel.code, sentinel.content)
-
-    try:
-        from refreshbooks.transports.use_urllib2 import Transport
-        Transport('x', dict)("foo")
-    except TransportException, e:
-        assert e.status is sentinel.code
-        assert e.content is sentinel.content
-    else:
-        assert False, "Didn't fire expected exception"
-
+@attr('integration')
+@raises(TransportException)
 def test_httplib2_transport_exception():
-    httplib2mock = Mock()
-    client = Mock()
-    httplib2mock.Http.return_value = client
-    client.request.return_value = (Mock(status=400), sentinel.content)
+    try:
+        import httplib2
+    except ImportError:
+        raise SkipTest("module 'httplib2' not installed")
+    from refreshbooks.transports.use_httplib2 import Transport
+    Transport('http://httpstat.us/400', dict)("foo")
 
-    with patch.dict('sys.modules', httplib2=httplib2mock):
-        try:
-            from refreshbooks.transports.use_httplib2 import Transport
-            Transport('x', dict)("foo")
-        except TransportException, e:
-            assert e.status == 400
-            assert e.content is sentinel.content
-        else:
-            assert False, "Didn't fire expected exception"
+@attr('integration')
+def test_httplib2():
+    try:
+        import httplib2
+    except ImportError:
+        raise SkipTest("module 'httplib2' not installed")
+    from refreshbooks.transports.use_httplib2 import Transport
+    assert len(Transport('http://httpstat.us/200', dict)("foo")) > 0
 
+@attr('integration')
+@raises(TransportException)
 def test_requests_transport_exception():
-    requestsmock = Mock()
-    requestsmock.post.return_value = Mock(status_code=400, content=sentinel.content)
+    try:
+        import requests
+    except ImportError:
+        raise SkipTest("module 'requests' not installed")
+    from refreshbooks.transports.use_requests import Transport
+    Transport('http://httpstat.us/400', dict)("foo")
 
-    with patch.dict('sys.modules', requests=requestsmock):
-        try:
-            from refreshbooks.transports.use_requests import Transport
-            Transport('x', dict)("foo")
-        except TransportException, e:
-            assert e.status == 400
-            assert e.content is sentinel.content
-        else:
-            assert False, "Didn't fire expected exception"
+@attr('integration')
+def test_requests():
+    try:
+        import requests
+    except ImportError:
+        raise SkipTest("module 'requests' not installed")
+    from refreshbooks.transports.use_requests import Transport
+    assert len(Transport('http://httpstat.us/200', dict)("foo")) > 0
