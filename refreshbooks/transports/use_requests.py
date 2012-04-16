1468cc6be65596da2f0042763c0b52d612a25de5
Taavi Burns <taavi@freshbooks.com>
2012-04-16 10:22:04 -0400

    We probably want to use keep-alive.

--- refreshbooks/transports/use_requests.py
+++ refreshbooks/transports/use_requests.py
@@ -4,12 +4,13 @@ from refreshbooks import exceptions as exc
 
 class Transport(object):
     def __init__(self, url, headers_factory):
+        self.session = requests.session()
         self.url = url
         self.headers_factory = headers_factory
     
     def __call__(self, entity):
         
-        resp = requests.post(
+        resp = self.session.post(
             self.url,
             headers=self.headers_factory(),
             data=entity
